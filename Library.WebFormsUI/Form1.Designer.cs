6d3f79fdcad44c5537f1cbc888301f6b72accdb9
kubrasnmez <sonmezkbra@gmail.com>
2021-03-21 09:48:26 +0300

    FormDesigner

--- Library.WebFormsUI/Form1.Designer.cs
+++ Library.WebFormsUI/Form1.Designer.cs
@@ -30,29 +30,98 @@ namespace Library.WebFormsUI
         private void InitializeComponent()
         {
             this.dgwbook = new System.Windows.Forms.DataGridView();
+            this.gbxCategory = new System.Windows.Forms.GroupBox();
+            this.gbxProductName = new System.Windows.Forms.GroupBox();
+            this.lblCategory = new System.Windows.Forms.Label();
+            this.cbxCategory = new System.Windows.Forms.ComboBox();
+            this.lblProductName = new System.Windows.Forms.Label();
+            this.tbxProductName = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgwbook)).BeginInit();
+            this.gbxCategory.SuspendLayout();
+            this.gbxProductName.SuspendLayout();
             this.SuspendLayout();
             // 
             // dgwbook
             // 
             this.dgwbook.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgwbook.Location = new System.Drawing.Point(12, 60);
+            this.dgwbook.Location = new System.Drawing.Point(12, 256);
             this.dgwbook.Name = "dgwbook";
             this.dgwbook.RowHeadersWidth = 51;
             this.dgwbook.RowTemplate.Height = 24;
             this.dgwbook.Size = new System.Drawing.Size(776, 150);
             this.dgwbook.TabIndex = 0;
             // 
+            // gbxCategory
+            // 
+            this.gbxCategory.Controls.Add(this.lblCategory);
+            this.gbxCategory.Controls.Add(this.cbxCategory);
+            this.gbxCategory.Location = new System.Drawing.Point(12, 12);
+            this.gbxCategory.Name = "gbxCategory";
+            this.gbxCategory.Size = new System.Drawing.Size(304, 95);
+            this.gbxCategory.TabIndex = 1;
+            this.gbxCategory.TabStop = false;
+            this.gbxCategory.Text = "Kategoriye Göre Ara";
+            // 
+            // gbxProductName
+            // 
+            this.gbxProductName.Controls.Add(this.tbxProductName);
+            this.gbxProductName.Controls.Add(this.lblProductName);
+            this.gbxProductName.Location = new System.Drawing.Point(12, 128);
+            this.gbxProductName.Name = "gbxProductName";
+            this.gbxProductName.Size = new System.Drawing.Size(304, 100);
+            this.gbxProductName.TabIndex = 2;
+            this.gbxProductName.TabStop = false;
+            this.gbxProductName.Text = "Ürün Adına Göre Ara";
+            // 
+            // lblCategory
+            // 
+            this.lblCategory.AutoSize = true;
+            this.lblCategory.Location = new System.Drawing.Point(6, 50);
+            this.lblCategory.Name = "lblCategory";
+            this.lblCategory.Size = new System.Drawing.Size(73, 17);
+            this.lblCategory.TabIndex = 0;
+            this.lblCategory.Text = "Kategori : ";
+            // 
+            // cbxCategory
+            // 
+            this.cbxCategory.FormattingEnabled = true;
+            this.cbxCategory.Location = new System.Drawing.Point(96, 50);
+            this.cbxCategory.Name = "cbxCategory";
+            this.cbxCategory.Size = new System.Drawing.Size(164, 24);
+            this.cbxCategory.TabIndex = 1;
+            // 
+            // lblProductName
+            // 
+            this.lblProductName.AutoSize = true;
+            this.lblProductName.Location = new System.Drawing.Point(6, 47);
+            this.lblProductName.Name = "lblProductName";
+            this.lblProductName.Size = new System.Drawing.Size(75, 17);
+            this.lblProductName.TabIndex = 0;
+            this.lblProductName.Text = "Ürün Adı : ";
+            // 
+            // tbxProductName
+            // 
+            this.tbxProductName.Location = new System.Drawing.Point(96, 47);
+            this.tbxProductName.Name = "tbxProductName";
+            this.tbxProductName.Size = new System.Drawing.Size(164, 22);
+            this.tbxProductName.TabIndex = 1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gbxProductName);
+            this.Controls.Add(this.gbxCategory);
             this.Controls.Add(this.dgwbook);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Ürünler";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgwbook)).EndInit();
+            this.gbxCategory.ResumeLayout(false);
+            this.gbxCategory.PerformLayout();
+            this.gbxProductName.ResumeLayout(false);
+            this.gbxProductName.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -60,6 +129,12 @@ namespace Library.WebFormsUI
         #endregion
 
         private System.Windows.Forms.DataGridView dgwbook;
+        private System.Windows.Forms.GroupBox gbxCategory;
+        private System.Windows.Forms.Label lblCategory;
+        private System.Windows.Forms.ComboBox cbxCategory;
+        private System.Windows.Forms.GroupBox gbxProductName;
+        private System.Windows.Forms.TextBox tbxProductName;
+        private System.Windows.Forms.Label lblProductName;
     }
 }
 
